a64e4902826b1038c2fb510372bdad54a4b46aab
Dylan KP <60126374+Dylankp101@users.noreply.github.com>
2020-11-30 15:58:16 -0800

    Initial commit

---
+++ YourJavaProgram.java
@@ -0,0 +1,7 @@
+public class YourJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Dylan");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is San Jose");
+    }
+}
